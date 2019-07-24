ca629abae7321d7ce57451d37f650c6c1a64ea7f
hammad-siraj <hammadsiraj224@gmail.com>
2019-07-25 01:56:03 +0500

    node-module example

--- index.js
+++ index.js
@@ -1,29 +1,25 @@
 // console.log("hello node")
 
-var rect = {
-
-    perimeter:(x,y) => (2*(x+y)),
-    area: (x,y) => (x*y) 
-}
+var rect = require("./rectangle");
 
 
 function AreaOfRectangle (l,b){
-console.log("we are finding area of rectangle length of :  "+l +" and bredth of :"+b )
-
-if(l<=0 || b<=0){
-    console.log("the dimension should be greater than 0")
+console.log("we are finding area of rectangle length of :  " +  l +" and bredth of :"+b )
+rect(l,b ,(err,rectangle)=>{
+if(err){
+console.log(err.message)
 }
 else{
-    console.log("the area of rectangle is : " + rect.area(l,b))
-
+    console.log(rectangle.area())
 }
-
-console.log("perimeter of rectangle  " + rect.perimeter(l,b))
+})
 
 }
 
 
 AreaOfRectangle(5,4);
-AreaOfRectangle(2,2);
+
+// AreaOfRectangle(2,2);
+
 AreaOfRectangle(0,0);
 
